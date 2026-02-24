58d3a2608e574e168be12221f297e5a8ece9b969
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2026-02-24 16:40:38 +0200

    fix: use SortedSet for deterministic validator iteration order
    DefaultValidators.all and DefaultMutators.all used Set which becomes
    HashSet for >4 elements, giving non-deterministic iteration order across
    JVM runs. This caused flaky integration test counts in
    LedgerRulesValidationTest because the first-matching validator varied.
    
    Switch to SortedSet ordered by validator name for stable iteration.

--- scalus-cardano-ledger/shared/src/main/scala/scalus/cardano/ledger/rules/DefaultValidators.scala
+++ scalus-cardano-ledger/shared/src/main/scala/scalus/cardano/ledger/rules/DefaultValidators.scala
@@ -1,11 +1,13 @@
 package scalus.cardano.ledger.rules
 
+import scala.collection.immutable.SortedSet
+
 /** Explicit list of all default validators for ledger rules validation.
   */
 object DefaultValidators {
 
     /** All built-in validators for Cardano ledger rules. */
-    val all: Set[STS.Validator] = Set(
+    val all: Set[STS.Validator] = SortedSet[STS.Validator](
       AllInputsMustBeInUtxoValidator,
       EmptyInputsValidator,
       ExactSetOfRedeemersValidator,
@@ -34,15 +36,15 @@ object DefaultValidators {
       WrongNetworkInTxBodyValidator,
       WrongNetworkValidator,
       WrongNetworkWithdrawalValidator
-    )
+    )(using Ordering.by(_.name))
 }
 
 /** Explicit list of all default mutators for ledger rules validation. */
 object DefaultMutators {
 
     /** All built-in mutators for Cardano ledger rules. */
-    val all: Set[STS.Mutator] = Set(
+    val all: Set[STS.Mutator] = SortedSet[STS.Mutator](
       PlutusScriptsTransactionMutator,
       StakeCertificatesMutator
-    )
+    )(using Ordering.by(_.name))
 }
